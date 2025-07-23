4d175e1e137a74ee159554d99d977ea678716d1b
urvari18 <138604926+urvari18@users.noreply.github.com>
2025-07-23 17:11:15 +0100

    Create app.test.js

---
+++ app.test.js
@@ -0,0 +1,11 @@
+// index.test.js
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /', () => {
+  it('should return Hello from DoorFeed!', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Hello from DoorFeed!');
+  });
+});

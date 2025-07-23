71ecb05094a34fda035b0c5035cfc0fe750dc274
urvari18 <138604926+urvari18@users.noreply.github.com>
2025-07-23 15:52:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,14 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "doorfeed-dev-app-bucket"
+  bucket = "doorfeed-${var.env}-app-bucket"
+}
+
+resource "aws_s3_bucket_acl" "app_bucket_acl" {
+  bucket = aws_s3_bucket.app_bucket.id
   acl    = "private"
 }
+
 
 output "bucket_name" {
   value = aws_s3_bucket.app_bucket.id
